ae667f811274e24d8ef55dfb03bd75df87f35705
Brian <yebri@umichedu>
2016-10-14 15:47:45 -0400

    yess

--- nosetests.py
+++ nosetests.py
@@ -1,10 +1,14 @@
 import unittest
 import rpn
 
-class TestExponent(unittest.TestCase):
-	def test_exponent(self):
-		result = rpn.calculate(2^3)
-		self.assertEqual(6, result)
+#class TestExponent(unittest.TestCase):
+	#def test_exponent(self):
+		#result = rpn.calculate(2^3)
+		#self.assertEqual(6, result)
+
+result - rpn.calculate(2 ^ 3)
+assert(result == 6)
+
 
 #x = 0^1
 #assert (x==1)
