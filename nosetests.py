0a35aada04d665c9c0abee0b6d383fc2b91b6c73
Brian <yebri@umichedu>
2016-10-14 15:44:13 -0400

    yes

--- nosetests.py
+++ nosetests.py
@@ -1,5 +1,13 @@
-x = 0^1
-assert (x==1)
+import unittest
+import rpn
+
+class TestExponent(unittest.TestCase):
+	def test_exponent(self):
+		result = rpn.calculate(2^3)
+		self.assertEqual(6, result)
+
+#x = 0^1
+#assert (x==1)
 #if x == 1:
 	#print "success"
 #else:
